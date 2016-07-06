ea4b9fd54485d3e77560d44a6015f355060c3c91
sakoh <sakohartounian@yahoo.com>
2016-07-06 01:55:56 -0700

    task moved to a separate file

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,6 @@
 'use strict';
 
 const gulp = require('gulp'),
-  nunjucks = require('gulp-nunjucks'),
-    marked = require('marked'),
-    rename = require("gulp-rename"),
-      path = require('path'),
-        gm = require('gray-matter'),
-        fs = require('fs');
+      templates = require('./tasks/templates');
 
-gulp.task('default', () => {
-
-  return fs.readdir('./content/', (err,files) => {
-
-    if(err) throw err;
-
-    files.forEach( file => {
-
-      const data = gm.read(`./content/${file}`);
-      const filename = path.basename(file, '.md');
-
-      data['content'] = marked(data['content']);
-
-      return gulp.src('templates/page.html')
-          .pipe(nunjucks.compile(data))
-          .pipe(rename(`${filename}.html`))
-          .pipe(gulp.dest('dist'))
-    });
-  });
-});
+gulp.task('templates', templates);
---
+++ tasks/templates.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const gulp = require('gulp'),
+  nunjucks = require('gulp-nunjucks'),
+    marked = require('marked'),
+    rename = require("gulp-rename"),
+      path = require('path'),
+        gm = require('gray-matter'),
+        fs = require('fs');
+
+module.exports = () => {
+
+  return fs.readdir('./content/', (err,files) => {
+
+    if(err) throw err;
+
+    files.forEach(file => {
+
+      const data = gm.read(`./content/${file}`),
+            filename = path.basename(file, '.md');
+
+      let dist;
+
+      if(filename !== "index") {
+        dist = `./dist/${filename}`
+      } else {
+        dist = './dist'
+      }
+
+
+      data['content'] = marked(data['content']);
+
+      return fs.mkdir(dist, () =>
+          gulp.src('templates/page.html')
+              .pipe(nunjucks.compile(data))
+              .pipe(rename('index.html'))
+              .pipe(gulp.dest(dist))
+      );
+
+    });
+
+  });
+}
