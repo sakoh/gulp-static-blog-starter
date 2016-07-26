90e6c74ca39455673b43b75fc36f2bb35f7cacd5
sakoh <sakohartounian@yahoo.com>
2016-07-25 23:12:55 -0700

    browser reload works

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const gulp = require("gulp"),
+      browserSync = require("browser-sync").create(),
       pages = require("./tasks/templates/pages"),
       posts = require("./tasks/templates/posts"),
       frontPage = require("./tasks/templates/front-page");
@@ -9,3 +10,15 @@ gulp.task("front-page", frontPage);
 gulp.task("pages", pages);
 gulp.task("posts", posts);
 gulp.task("default", ["pages", "posts", "front-page"]);
+
+gulp.task("watch", () => {
+  const reload = browserSync.reload;
+
+    browserSync.init({
+        server: {
+            baseDir: "./dist"
+        }
+    });
+
+    gulp.watch("./templates/**/*.html",["default", reload]);
+});
