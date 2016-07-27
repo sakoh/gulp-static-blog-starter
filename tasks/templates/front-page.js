8198629b9c6b6808d2b4f6dd6522176ff3ce4efa
sakoh <sakohartounian@yahoo.com>
2016-07-26 23:14:28 -0700

    frontPage task has been reformatted

--- tasks/templates/front-page.js
+++ tasks/templates/front-page.js
@@ -2,22 +2,11 @@
 
 const gulp = require("gulp"),
       nunjucks = require("gulp-nunjucks"),
-      marked = require("marked"),
       rename = require("gulp-rename"),
-      assign = require("lodash").assign,
-      path = require("path"),
-      data = require("../../data"),
-      gm = require("gray-matter"),
-      fs = require("fs"),
-      q = require("q");
+      data = require("../../data");
 
-module.exports = () => {
-    return gulp.src("templates/front-page.html")
-        .pipe(
-          nunjucks.compile(
-            assign(data)
-          )
-        )
-        .pipe(rename("index.html"))
-        .pipe(gulp.dest('dist'));
-}
+module.exports = () =>
+  gulp.src("templates/front-page.html")
+      .pipe(nunjucks.compile(data))
+      .pipe(rename("index.html"))
+      .pipe(gulp.dest('dist'));
